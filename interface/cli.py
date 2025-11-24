06b8d105384e77fbe42d36a486f29687c32eb0ea
stanc <stancuanamaria0222@gmail.com>
2025-11-24 16:20:56 +0200

    Commit

--- interface/cli.py
+++ interface/cli.py
@@ -1,42 +1,50 @@
 from services.rezervari_service import creeaza_rezervare, sterge_rezervare
-from services.admin_service import adauga_film, adauga_sala
+from services.admin_service import adauga_sala
+from services.api_filme_service import adauga_film_din_api
 
 
 def meniu():
     while True:
         print("\n=== MENIU CINEMA ===")
-        print("1. Adaugă film")
-        print("2. Adaugă sală")
-        print("3. Creează rezervare")
-        print("4. Anulează rezervare")
+        print("1. Adaugă sală")
+        print("2. Creează rezervare")
+        print("3. Anulează rezervare")
+        print("4. Adaugă film din API")
         print("0. Ieșire")
 
         opt = input("Alege opțiunea: ")
 
         if opt == "1":
-            titlu = input("Titlu film: ")
-            durata = int(input("Durata: "))
-            sala_id = int(input("ID sala: "))
-            adauga_film(1, titlu, durata, sala_id)
-
-        elif opt == "2":
             nume = input("Nume sală: ")
             randuri = int(input("Randuri: "))
             locuri = int(input("Locuri pe rând: "))
-            adauga_sala(1, nume, randuri, locuri)
+            sala = adauga_sala(nume, randuri, locuri)
+            print(f"Sală adăugată cu ID: {sala['id_sala']}")
 
-        elif opt == "3":
+        elif opt == "2":
             film = int(input("ID film: "))
             sala = int(input("ID sala: "))
             r = int(input("Rand: "))
             l = int(input("Loc: "))
-            print(creeaza_rezervare(film, sala, r, l))
+            rezervare = creeaza_rezervare(film, sala, r, l)
+            print("Rezervare creată:", rezervare)
 
-        elif opt == "4":
-            rez_id = int(input("ID rezervare: "))
+        elif opt == "3":
+            rez_id = int(input("ID rezervare de anulat: "))
             sterge_rezervare(rez_id)
 
+        elif opt == "4":
+            titlu = input("Titlul filmului: ")
+            sala_id = int(input("ID sala: "))
+            rezultat = adauga_film_din_api(titlu, sala_id)
+            if rezultat:
+                print("Film adăugat automat din API cu succes!")
+            else:
+                print("Nu s-a putut adăuga filmul.")
+
         elif opt == "0":
+            print("La revedere!")
             break
+
         else:
             print("Opțiune invalidă!")
