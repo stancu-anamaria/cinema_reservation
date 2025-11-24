f8890159f00f577a84bab47ed80c7e08f5aad6ef
mazilu <mazilustefania85@gmail.com>
2025-11-24 16:50:04 +0200

    aplicatie aproape

--- services/admin_service.py
+++ services/admin_service.py
@@ -1,8 +1,47 @@
 import json
+import os
 
-FILME_FILE = "data/filme.json"
-SALI_FILE = "data/sali.json"
+DATA_DIR = "data"
+FILME_FILE = os.path.join(DATA_DIR, "filme.json")
+SALI_FILE = os.path.join(DATA_DIR, "sali.json")
 
+
+# -------------------- SALI -----------------------
+
+def incarca_sali():
+    try:
+        with open(SALI_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+
+def salveaza_sali(lista):
+    with open(SALI_FILE, "w") as f:
+        json.dump(lista, f, indent=4)
+
+
+def genereaza_id_sala():
+    sali = incarca_sali()
+    if not sali:
+        return 1
+    return sali[-1]["id_sala"] + 1
+
+
+def adauga_sala(id_sala, nume, randuri, locuri_pe_rand):
+    sali = incarca_sali()
+    sala = {
+        "id_sala": id_sala,
+        "nume": nume,
+        "randuri": randuri,
+        "locuri_pe_rand": locuri_pe_rand
+    }
+    sali.append(sala)
+    salveaza_sali(sali)
+    return sala
+
+
+# -------------------- FILME -----------------------
 
 def incarca_filme():
     try:
@@ -21,7 +60,7 @@ def genereaza_id_film():
     filme = incarca_filme()
     if not filme:
         return 1
-    return max(f["id_film"] for f in filme) + 1
+    return filme[-1]["id_film"] + 1
 
 
 def adauga_film(titlu, durata, sala_id):
